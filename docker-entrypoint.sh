ea297df67de8683ccda692f20981014a35148557
tagliateller <tagliateller@users.noreply.github.com>
2016-11-26 13:13:30 +0100

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,2 +1,7 @@
 #!/bin/sh
 
+# Datenbank anlegen
+mysqladmin -u root --password=my-secret-pw -h $MYSQL_PORT_3306_TCP_ADDR create tpcc3000
+
+# tpcc-load ausführen
+./tpcc_load -h $MYSQL_PORT_3306_TCP_ADDR -d tpcc3000 -u root -p "my-secret-pw" -w 10
