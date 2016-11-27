53d5e75cddcdc6eaf6c07e8b06b56187a6c58964
tagliateller <tagliateller@users.noreply.github.com>
2016-11-27 12:58:57 +0100

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
 
-# Test - welcher Nutzer ?
-whoami
-
 # Datenbank anlegen
 echo "Anlegen der Datenbank"
 mysqladmin -u root --password=my-secret-pw -h $MYSQL_PORT_3306_TCP_ADDR create tpcc3000
@@ -10,10 +7,10 @@ echo "Datenbank wurde angelegt"
 
 # Laden der Struktur
 echo "Laden des Metaschema"
-mysql -u root --password=my-secret-pw -h $MYSQL_PORT_3306_TCP_ADDR -D tpcc3000 < /home/root/tpcc-mysql/create_table.sql
+mysql -u root --password=my-secret-pw -h $MYSQL_PORT_3306_TCP_ADDR -D tpcc3000 < /opt/tpcc-mysql/create_table.sql
 echo "Metaschema wurde geladen"
 
 # tpcc-load ausführen
 echo "Daten werden geladen"
-/home/root/tpcc-mysql/tpcc_load -h $MYSQL_PORT_3306_TCP_ADDR -d tpcc3000 -u root -p "my-secret-pw" -w 10
+/opt/tpcc-mysql/tpcc_load -h $MYSQL_PORT_3306_TCP_ADDR -d tpcc3000 -u root -p "my-secret-pw" -w 10
 echo "Daten wurden geladen"
